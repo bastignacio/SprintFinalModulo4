dc5e35d3a8339d27dfd58b77ab2ba3ec1422c956
bastiard0 <bastignacio@gmail.com>
2024-08-27 23:42:55 -0400

    Eliminacion

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"sprintFinal","l":"sprintFinal"}];updateSearchResults();
--- doc/tag-search-index.js
+++
@@ -1,1 +0,0 @@
-tagSearchIndex = [{"l":"Este es el JavaDoc de la tarea Grupal 12 del Modulo 04","h":"Overview","d":"Section","u":"index.html#este-es-el-javadoc-de-la-tarea-grupal-12-del-modulo-04-heading"}];updateSearchResults();
